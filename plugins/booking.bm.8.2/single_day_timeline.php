1912fc25ae094147e12b174babd387307bc74ae6
Tyler Giordani <tgiordani567@gmail.com>
2018-06-28 12:23:41 -0400

    Cleaned up the dynamic timelime hour dispay function -- Much less code

--- plugins/booking.bm.8.2/single_day_timeline.php
+++ plugins/booking.bm.8.2/single_day_timeline.php
@@ -1,31 +1,22 @@
 <?php
 function todays_sim_timeline() {
-  $dayofweek = date('w', strtotime($date));
 
+  $dayofweek = date('w', strtotime($date));
+  $hours = '7,';
   // Prints a timeline showing the current hours the restaurant is opne for that day
   // '0' = sunday, '1' = monday, ect...
-
-  if ($dayofweek = 0){
-    ?>
-    <div>
-      <?php echo do_shortcode("[bookingtimeline limit_hours='7,22' type='5,6,7,8,9,10,11,12' view_days_num=1 header_title='Todays Simulator Availability']"); ?>
-    </div>
-    <?php
+  if($dayofweek == 0){
+    $hours = $hours . '22';
   }
-  elseif ($dayofweek > 0 && $dayofweek < 5) {
-    ?>
-    <div>
-      <?php echo do_shortcode("[bookingtimeline limit_hours='7,23' type='5,6,7,8,9,10,11,12' view_days_num=1 header_title='Todays Simulator Availability']"); ?>
-    </div>
-    <?php
+  elseif(($dayofweek > 0) && ($dayofweek < 5)){
+    $hours = $hours . '23';
   }
   else {
-    ?>
-    <div>
-      <?php echo do_shortcode("[bookingtimeline limit_hours='7,24' type='5,6,7,8,9,10,11,12' view_days_num=1 header_title='Todays Simulator Availability']"); ?>
-    </div>
-    <?php
+    $hours = $hours . '24';
   }
+
+  echo do_shortcode("[bookingtimeline limit_hours='$hours' type='5,6,7,8,9,10,11,12' view_days_num=1 header_title='Todays Simulator Availability']");
+
 }
 
 add_shortcode( 'todays_sim_bookings', 'todays_sim_timeline');
